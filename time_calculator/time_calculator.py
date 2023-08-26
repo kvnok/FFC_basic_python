2cae5ddf1674def48a0e9837a056e6975c661196
Kevin Kroon <kkroon@student.codam.nl>
2023-08-26 12:37:44 +0200

    parsing

--- time_calculator/time_calculator.py
+++ time_calculator/time_calculator.py
@@ -1,8 +1,32 @@
-def add_time(start, duration, day=None):
-    new_time = ""
-    if day is None:
-        print("2 args")
-    else:
-        print("3 args")
+def add_time(start, duration, day_of_week=None):
+	#base vars
+	new_time = ""
+	days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+	#extract
+	begin = start.split(":")
+	begin[1] = begin[1].split(" ")
+	mode = begin[1][1]
+	add = duration.split(":")
+	#convert
+	big = int(begin[0]) + int(add[0])
+	small = int(begin[1][0]) + int(add[0])
+	#adjust
+	if small >= 60:
+		big += 1
+		small -= 60
+	if mode == "AM":
+		big += 12
+	print("BIG " + str(big) + " SMALL " + str(small))
+	if day_of_week is None: #no day given
+		print("if")
+	else: #a day is given
+		day = day_of_week
+		print("else")
+		index = 0
+		for entry in days:
+			if entry.lower() == day.lower():
+				print(day)
+				break
+			index += 1
 
-    return new_time
+	return new_time
