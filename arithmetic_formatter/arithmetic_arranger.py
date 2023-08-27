5ed7c9fe75adec48888b9e9c54a4f47eba4f7de5
Kevin Kroon <kkroon@student.codam.nl>
2023-08-27 18:02:35 +0200

    added class and some base stuff

--- arithmetic_formatter/arithmetic_arranger.py
+++ arithmetic_formatter/arithmetic_arranger.py
@@ -1,4 +1,23 @@
-def arithmetic_arranger(problems):
-	arranged_problems = ""
+class Problem:
+	def __init__(self):
+		self.n1 = None
+		self.n2 = None
+		self.op = None
 
-    return arranged_problems
+def arithmetic_arranger(problems, display=None):
+	arranged_problems = ""
+	error_strings = ["Error: Too many problems.", "Error: Operator must be '+' or '-'.",
+					"Error: Numbers must only contain digits.", "Error: Numbers cannot be more than four digits."]
+	#errorchecking
+	problem_count = 0
+	for entry in problems:
+		problem_count += 1
+		if problem_count == 5:
+			return error_strings[0]
+		#array of Problem, filling it and errorchecking it for symbols and length
+
+	if display is not None:
+		if display is True:
+			print("display is set to True")
+
+	return arranged_problems
--- arithmetic_formatter/main.py
+++ arithmetic_formatter/main.py
@@ -1,5 +1,7 @@
-from pytest import main
+# from pytest import main
 from arithmetic_arranger import arithmetic_arranger
 # main(['-vv'])
+#need to uncomment above when turning in
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["1", "2", "3", "4", "5", "6"]))
