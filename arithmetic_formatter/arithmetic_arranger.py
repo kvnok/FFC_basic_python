c2502bcf3a13afb65d08611db683725246dc3b05
Kevin Kroon <kkroon@student.codam.nl>
2023-08-27 19:46:01 +0200

    errorchecking done

--- arithmetic_formatter/arithmetic_arranger.py
+++ arithmetic_formatter/arithmetic_arranger.py
@@ -1,11 +1,12 @@
 class Problem:
-	def __init__(self):
-		self.n1 = None
-		self.n2 = None
-		self.op = None
+	def __init__(self, n1, op, n2):
+		self.n1 = n1
+		self.n2 = n2
+		self.op = op
 
 def arithmetic_arranger(problems, display=None):
 	arranged_problems = ""
+	problem_arr = []
 	error_strings = ["Error: Too many problems.", "Error: Operator must be '+' or '-'.",
 					"Error: Numbers must only contain digits.", "Error: Numbers cannot be more than four digits."]
 	#errorchecking
@@ -14,10 +15,17 @@ def arithmetic_arranger(problems, display=None):
 		problem_count += 1
 		if problem_count == 5:
 			return error_strings[0]
-		#array of Problem, filling it and errorchecking it for symbols and length
+		entry = entry.split(" ")
+		problem_arr.append(Problem(entry[0], entry[1], entry[2]))
+		if entry[1] != '+' and entry[1] != '-':
+			return error_strings[1]
+		if entry[0].isdigit() is not True or entry[2].isdigit() is not True:
+			return error_strings[2]
+		if len(entry[0]) > 4 or len(entry[2]) > 4:
+			return error_strings[3]
 
 	if display is not None:
 		if display is True:
 			print("display is set to True")
-
+	return "test"
 	return arranged_problems
--- arithmetic_formatter/main.py
+++ arithmetic_formatter/main.py
@@ -1,7 +1,14 @@
 # from pytest import main
 from arithmetic_arranger import arithmetic_arranger
 # main(['-vv'])
-#need to uncomment above when turning in
 
+#need to uncomment above when turning in
+#python3 main.py
+# print(arithmetic_arranger(["1", "2", "3", "4", "5", "6"]))
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
-print(arithmetic_arranger(["1", "2", "3", "4", "5", "6"]))
+
+#error tests
+# print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49", "1345 + 632", "1734 - 215"]))
+# print(arithmetic_arranger(["32 / 698", "3801 - 2", "45 + 43", "123 + 49"]))
+# print(arithmetic_arranger(["32 + 69bruh8", "3801 - 2", "45 + 43", "123 + 49"]))
+# print(arithmetic_arranger(["323333333 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
