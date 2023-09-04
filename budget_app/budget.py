be4ab2455a8ce8e1ed2608c423b65ff0b530efb3
Kevin Kroon <kevinjankroon@gmail.com>
2023-09-05 01:25:11 +0200

    spacing

--- budget_app/budget.py
+++ budget_app/budget.py
@@ -15,41 +15,48 @@ class Category:
 	"""
 	def __init__(self, name):
 		self.n1 = name
-		ledger = []
+		self.ledger = []
+		self.balance = 0.0
 	"""
 	deposit method that accepts an amount and description.
 	If no description is given, it should default to an empty string.
-	The method should append an object to the ledger list in the form of {"amount": amount, "description": description}.
+	The method should append an object to the ledger list,
+	in the form of {"amount": amount, "description": description}.
 	"""
-	def deposit(amount, description, self):
+	def deposit(self, amount, description=""):
 		print("yep")
 	"""
-	withdraw method that is similar to the deposit method, but the amount passed in should be stored in the ledger as a negative number.
+	withdraw method that is similar to the deposit method,
+	but the amount passed in should be stored in the ledger as a negative number.
 	If there are not enough funds, nothing should be added to the ledger.
 	This method should return True if the withdrawal took place, and False otherwise.
 	"""
-	def withdraw(amount, description=None, self):
+	def withdraw(self, amount, description=""):
 		print("yep")
 	"""
-	get_balance method that returns the current balance of the budget category based on the deposits and withdrawals that have occurred.
+	get_balance method that returns the current balance of the budget category,
+	based on the deposits and withdrawals that have occurred.
 	"""
 	def get_balance(self):
 		print("yep")
 	"""
 	transfer method that accepts an amount and another budget category as arguments.
-	The method should add a withdrawal with the amount and the description "Transfer to [Destination Budget Category]".
-	The method should then add a deposit to the other budget category with the amount and the description "Transfer from [Source Budget Category]".
+	The method should add a withdrawal with the amount
+	and the description "Transfer to [Destination Budget Category]".
+	The method should then add a deposit to the other budget category, 
+	with the amount and the description "Transfer from [Source Budget Category]".
 	If there are not enough funds, nothing should be added to either ledgers.
 	This method should return True if the transfer took place, and False otherwise.
 	"""
-	def transfer(amount, category, self):
+	def transfer(self, amount, category):
 		print("yep")
 	"""
 	check_funds method that accepts an amount as an argument.
-	It returns False if the amount is greater than the balance of the budget category and returns True otherwise.
+	It returns False if the amount is greater than the balance of the budget category,
+	and returns True otherwise.
 	This method should be used by both the withdraw method and transfer method.
 	"""
-	def check_funds(amount, self):
+	def check_funds(self, amount):
 		print("yep")
 
 """
